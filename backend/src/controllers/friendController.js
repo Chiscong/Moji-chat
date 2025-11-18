279c598b2b46f6919c9650e39c80abe45aabd121
Chiscong <144198618+Chiscong@users.noreply.github.com>
2025-11-18 22:03:31 +0700

    feat(BE): feature accept friend request

--- backend/src/controllers/friendController.js
+++ backend/src/controllers/friendController.js
@@ -24,7 +24,7 @@ export const sendFriendRequest = async (req, res) => {
             Friend.findOne({ userA, userB }),
             FriendRequest.findOne({
                 $or: [
-                    {from,to},
+                    { from, to },
                     { from: to, to: from },
                 ]
             })
@@ -44,7 +44,30 @@ export const sendFriendRequest = async (req, res) => {
 }
 export const acceptFriend = async (req, res) => {
     try {
-
+        const { requestId } = req.params;
+        const userId = req.user._id;
+        // Kiểm tra yêu cầu kết bạn có tồn tại không
+        const request = await FriendRequest.findById(requestId);
+        if (!request) {
+            return res.status(404).json({ message: "Yêu cầu kết bạn không tồn tại" });
+        }
+        // Kiểm tra người dùng có quyền chấp nhận yêu cầu này không
+        if (request.to.toString() !== userId) {
+            return res.status(403).json({ message: "Bạn không có quyền chấp nhận yêu cầu này" });
+        }
+        // Tạo mối quan hệ bạn bè
+        const friend = await Friend.create({ userA: request.from, userB: request.to });
+        // Xóa yêu cầu kết bạn sau khi đã chấp nhận
+        await FriendRequest.findByIdAndDelete(requestId);
+        const from = await User.findById(request.from).select("_id displayName avatarUrl").lean();
+        res.status(200).json({ 
+            message: "Đã chấp nhận yêu cầu kết bạn",
+            newFriend: {
+                _id: from?._id,
+                displayName: from?.displayName,
+                avatarUrl: from?.avatarUrl
+            }
+            });
     } catch (error) {
         console.error("Lỗi khi chấp nhận yêu cầu kết bạn", error);
         res.status(500).json({ message: "Internal server error" });
@@ -52,7 +75,7 @@ export const acceptFriend = async (req, res) => {
 }
 export const declineFriendRequest = async (req, res) => {
     try {
-
+        
     } catch (error) {
         console.error("Lỗi khi từ chối yêu cầu kết bạn", error);
         res.status(500).json({ message: "Internal server error" });
