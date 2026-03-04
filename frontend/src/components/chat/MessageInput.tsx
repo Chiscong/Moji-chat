0501ce81716cb5804cc5b960e47c810bd8b15bf0
Chiscong <144198618+Chiscong@users.noreply.github.com>
2026-03-05 01:07:37 +0700

    feat(FE): add MessageInput component

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useAuthStore } from "@/stores/useAuthStore";
+import type { Conversation } from "@/types/chat";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { ImagePlus, Send } from "lucide-react";
+import { Input } from "../ui/input";
 
-const MessageInput = () => {
+const MessageInput = ({ selectedConvo }: { selectedConvo: Conversation }) => {
+  const { user } = useAuthStore();
+  const [value, setValue] = useState("");
+  if (!user) return null;
   return (
-    <div>MessageInput</div>
-  )
-}
+    <div className="flex items-center gap-2 p-3 min-h-[56px] bg-background">
+      <Button
+        variant="ghost"
+        size="icon"
+        className="hover:bg-primary/10 transition-smooth"
+      >
+        <ImagePlus className="size-4"></ImagePlus>
+      </Button>
+      <div className="flex-1 relative ">
+        <Input
+          placeholder="Soạn tin nhắn..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="pr-20 h-9 bg-white border-border/50 focus:border-primary/50 transition-smooth resize-none"
+        ></Input>
+        <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center gap-1">
+          <Button
+            asChild
+            variant="ghost"
+            size="icon"
+            className="hover:bg-primary/10 transition-smooth"
+          >
+            <div>{/* emoji picker */}</div>
+          </Button>
+        </div>
+      </div>
+      <Button
+        className="bg-gradient-chat hover:shadow-glow transition-smooth hover:scale-105"
+        disabled={!value.trim()}
+      >
+        <Send className="size-4 text-white" />
+      </Button>
+    </div>
+  );
+};
 
-export default MessageInput
+export default MessageInput;
